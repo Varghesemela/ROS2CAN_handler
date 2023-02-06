ed3de49f618bf6dffaaf0b3def4fe63e74fc8580
sanjay <sanjay@Ubuntu22.04.myguest.virtualbox.org>
2023-02-06 07:00:53 +0530

    [Sanjay] Added publisher to send dummy data

--- src/ros2can_handler/src/ros2can_handler.cpp
+++ src/ros2can_handler/src/ros2can_handler.cpp
@@ -1,5 +1,6 @@
 #include "rclcpp/rclcpp.hpp"
-#include "example_interfaces/msg/int64.hpp"
+// #include "example_interfaces/msg/int64.hpp"
+#include "can_msg_frame/msg/frame.hpp"
 
 class CANHandlerNode : public rclcpp::Node
 {
@@ -7,21 +8,31 @@ class CANHandlerNode : public rclcpp::Node
         CANHandlerNode() : Node("ros2can_handler")
         {
             RCLCPP_INFO(this->get_logger(), "Publisher node is initialised");
-            publisher_ = this->create_publisher<example_interfaces::msg::Int64>("CAN_transmit", 10);
-            timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&CANHandlerNode::publish_CAN_data, this));
+            // publisher_ = this->create_publisher<example_interfaces::msg::Int64>("CAN_transmit", 10);
+
+            publisher_ 	= this->create_publisher<can_msg_frame::msg::Frame>("CAN_transmit", 10);
+            // subscription_ 	= this->create_subscription<can_msgs::msg::Frame>("CAN_receive", std::bind(&ros2socketcan::CanPublisher, this, _1));
+
+            timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&CANHandlerNode::CANPublisher, this));
             
         }
 
     private:
 
-        void publish_CAN_data(){
-           auto msg = example_interfaces::msg::Int64();
-           counter_ += 1;
-           msg.data = counter_;
-           publisher_->publish(msg);
+        void CANPublisher(){
+            auto msg = can_msg_frame::msg::Frame();
+            msg.header.stamp = this->get_clock()->now();
+            msg.id  = 0x101;
+            msg.dlc = 8;
+            msg.eff = 0x11;
+            msg.rtr = 0x1;
+            msg.err = 0x0;
+            msg.data[0]= counter_;
+            counter_++;
+            publisher_->publish(msg);
         }
 
-        rclcpp::Publisher<example_interfaces::msg::Int64>::SharedPtr publisher_;
+        rclcpp::Publisher<can_msg_frame::msg::Frame>::SharedPtr publisher_;
         rclcpp::TimerBase::SharedPtr timer_;
         int counter_ = 0;
 };
